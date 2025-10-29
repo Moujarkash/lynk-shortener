5b3e11f785a9d2b952b9411144b6c0f2ff38659d
Mod <mouhmmad488@yahoo.com>
2025-10-29 21:59:37 +0300

    refactor Monitoring config: replace Dropwizard with Micrometer and update Prometheus setup, bump swagger-codegen to 3.0.71

--- src/main/kotlin/plugins/Monitoring.kt
+++ src/main/kotlin/plugins/Monitoring.kt
@@ -1,28 +1,18 @@
 package com.mod.plugins
 
-import com.codahale.metrics.*
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.metrics.dropwizard.*
 import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.calllogging.*
-import io.micrometer.prometheus.*
-import java.util.concurrent.TimeUnit
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 
 fun Application.configureMonitoring() {
     val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
     
     install(MicrometerMetrics) {
         registry = appMicrometerRegistry
-    }
-    install(DropwizardMetrics) {
-        Slf4jReporter.forRegistry(registry)
-            .outputTo(this@configureMonitoring.log)
-            .convertRatesTo(TimeUnit.SECONDS)
-            .convertDurationsTo(TimeUnit.MILLISECONDS)
-            .build()
-            .start(10, TimeUnit.SECONDS)
     }
     install(CallId) {
         header(HttpHeaders.XRequestId)
